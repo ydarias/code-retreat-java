a0b8a9a560c59c6120e4e6f355be543e3826a7fe
Yeray Darias <ydarias@gmail.com>
2019-09-18 15:39:35 +0200

    Autoformat of files to keep style

--- src/main/java/code/retreat/java/Cell.java
+++ src/main/java/code/retreat/java/Cell.java
@@ -4,16 +4,16 @@ public class Cell {
 
     private boolean alive;
 
-	public void evolveWith(int neighbours) {
+    public void evolveWith(int neighbours) {
         alive = true;
-	}
+    }
 
     public boolean isDead() {
         return !alive;
     }
 
-	public boolean isAlive() {
-		return alive;
-	}
+    public boolean isAlive() {
+        return alive;
+    }
 
 }
